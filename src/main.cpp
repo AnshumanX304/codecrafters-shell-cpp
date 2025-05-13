8a48a1082838650b935def45714e8819454650d8
Anshuman Tiwari <anshumantiwari21153045@gmail.com>
2025-05-14 01:34:22 +0530

    added echo command

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,11 @@ int main() {
   if(input == "exit 0") {
     return 0;
   }
-  std::cout << input << ": command not found" << std::endl;
+  else if(input.substr(0,4) == "echo"){
+    std::cout << input.substr(6)<< std::endl;
+  }
+  else{
+    std::cout << input << ": command not found" << std::endl;
+  }
   main();
 }
