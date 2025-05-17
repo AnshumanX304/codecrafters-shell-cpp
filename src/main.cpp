c3ac0ad28347b51ebfae807d68b997cc048720a4
Anshuman Tiwari <anshumantiwari21153045@gmail.com>
2025-05-18 01:09:00 +0530

    added path builtin

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <cstdlib>    // for std::getenv
+#include <fstream>    // for std::ifstream
+
+using namespace std;
 
 int main() {
-  // Flush after every std::cout / std:cerr
+  // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
@@ -14,20 +19,52 @@ int main() {
   if(input == "exit 0") {
     return 0;
   }
-  else if(input.substr(0,4) == "echo"){
-    std::cout << input.substr(5)<< std::endl;
+  else if (input.substr(0, 4) == "echo") {
+    std::cout << input.substr(5) << std::endl;
   }
-  else if(input.substr(0,4) == "type"){
-    std::string data=input.substr(5);
-    if(data == "echo" || data == "exit" || data == "type"){
-      std::cout<<data<<" is a shell builtin"<<std::endl;
+  else if (input.substr(0, 4) == "type") {
+    std::string data = input.substr(5);
+    if (data == "echo" || data == "exit" || data == "type") {
+      std::cout << data << " is a shell builtin" << std::endl;
     }
-    else{
-      std::cout<<data<<": not found"<<std::endl;
+    else {
+      const char* path_env = std::getenv("PATH");
+      std::string path_str(path_env);
+      string curr = "";
+      vector<string> paths;
+
+      for (int i = 0; i < path_str.size(); i++) {
+        if (path_str[i] == ':') {
+          paths.push_back(curr);
+          curr = "";
+        }
+        else {
+          curr += path_str[i];
+        }
+      }
+      if (!curr.empty()) {
+        paths.push_back(curr); 
+      }
+
+      bool found = false;
+      for (auto& it : paths) {
+        string newpath = it + "/" + data;
+        std::ifstream file(newpath);
+        if (file.good()) {
+          std::cout << data << " is " << newpath << std::endl;
+          found = true;
+          break;
+        }
+      }
+
+      if (!found) {
+        std::cout << data << ": not found" << std::endl;
+      }
     }
   }
-  else{
+  else {
     std::cout << input << ": command not found" << std::endl;
   }
-  main(); 
+
+  main(); // continue shell loop
 }
